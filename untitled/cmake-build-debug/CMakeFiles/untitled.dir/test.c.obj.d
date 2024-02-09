632e0f5d56e977fe01781ba0dcceba6821a777ae
Sun-Zhiyuan <1412003552@qq.com>
2024-02-09 22:15:16 +0800

    C++

---
+++ untitled/cmake-build-debug/CMakeFiles/untitled.dir/test.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/untitled.dir/test.c.obj: \
+ C:\Users\zhiyuan\CLionProjects\untitled\test.c \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Users/zhiyuan/Downloads/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h
